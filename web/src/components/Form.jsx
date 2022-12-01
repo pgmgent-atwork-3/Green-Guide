1e20b9c9c06530c403f7968c86d7432db683a3b5
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-12-01 15:28:05 +0100

    Updated Form - learning Formik and Yup

--- web/src/components/Form.jsx
+++ web/src/components/Form.jsx
@@ -47,21 +47,24 @@ function Form() {
                     <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
                         <div className="grid grid-cols-3 gap-6">
                             <div className="col-span-3 sm:col-span-2">
-                                <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
-                                    test
-                                </label>
                                 <div className="mt-1 flex rounded-md shadow-sm"></div>
+                                <label htmlFor="company-website" className="block text-sm font-bold text-gray-700">
+                                    E-mail address
+                                </label>
                                 <input
-                                type="email"
-                                name="email"
-                                id="email"
-                                placeholder="hello@example.com"
-                                className="p-3 mt-1 block w-full rounded-md bg-gray-50 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.email}
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    placeholder="hello@example.com"
+                                    className="p-3 mt-1 block w-full rounded-md bg-gray-50 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.email}
                                 />
-                                {errors.email && touched.email && errors.email}
+                                <small className="pb-4 block text-accent-100">{errors.email && touched.email && errors.email}</small>
+                                <label htmlFor="company-website" className="block text-sm font-bold text-gray-700">
+                                    Password
+                                </label>
                                 <input
                                     type="password"
                                     name="password"
@@ -72,15 +75,19 @@ function Form() {
                                     onBlur={handleBlur}
                                     value={values.password}
                                 />
+                                <small className="pb-4 block text-accent-100">{errors.password && touched.password && errors.password}</small>
                                 </div>
                             </div>
+                    <button
+                        type="submit"
+                        disabled={isSubmitting}
+                        className="inline-flex justify-center rounded-md border border-transparent bg-orange-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                        Save
+                    </button>
                         </div>
                     </div>
-                
-                {errors.password && touched.password && errors.password}
-                <button type="submit" disabled={isSubmitting}>
-                    Submit
-                </button>
+
             </form>
         )}
     </Formik>
