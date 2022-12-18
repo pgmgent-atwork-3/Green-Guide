4c93b62d718861cc016af2cebd253b4b4d2522f5
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2022-12-18 16:56:48 +0100

    added typing and security to contact-person endpoint

--- backend/src/contact-person/contact-person.resolver.ts
+++ backend/src/contact-person/contact-person.resolver.ts
@@ -3,15 +3,25 @@ import { ContactPersonService } from './contact-person.service';
 import { ContactPerson } from './entities/contact-person.entity';
 import { CreateContactPersonInput } from './dto/create-contact-person.input';
 import { UpdateContactPersonInput } from './dto/update-contact-person.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/gql-auth.guard';
+import { RolesGuard } from '../auth/role.guard';
+import { Roles } from '../Decorators/roles.decorator';
+import { Role } from '../role.enum';
+import { CurrentUser } from '../Decorators/currentUser.decorator';
+import { User } from '../user/entities/user.entity';
 
 @Resolver(() => ContactPerson)
 export class ContactPersonResolver {
   constructor(private readonly contactPersonService: ContactPersonService) {}
 
   @Mutation(() => ContactPerson)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.COMPANY)
   createContactPerson(
     @Args('createContactPersonInput')
     createContactPersonInput: CreateContactPersonInput,
+    @CurrentUser() user: User,
   ): Promise<ContactPerson> {
     return this.contactPersonService.create(createContactPersonInput);
   }
@@ -27,17 +37,23 @@ export class ContactPersonResolver {
   }
 
   @Mutation(() => ContactPerson)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.COMPANY)
   updateContactPerson(
     @Args('id', { type: () => Int }) id: number,
     @Args('updateContactPersonInput')
     updateContactPersonInput: UpdateContactPersonInput,
+    @CurrentUser() user: User,
   ): Promise<ContactPerson> {
     return this.contactPersonService.update(id, updateContactPersonInput);
   }
 
   @Mutation(() => ContactPerson)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.COMPANY)
   removeContactPerson(
     @Args('id', { type: () => Int }) id: number,
+    @CurrentUser() user: User,
   ): Promise<ContactPerson> | null {
     return this.contactPersonService.remove(id);
   }
