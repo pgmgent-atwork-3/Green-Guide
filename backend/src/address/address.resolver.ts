1c8b58fb6a3dd5ce9df53be933b0e792378a0ba3
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2022-12-18 17:47:22 +0100

    added typing and security to address endpoint

--- backend/src/address/address.resolver.ts
+++ backend/src/address/address.resolver.ts
@@ -3,14 +3,24 @@ import { AddressService } from './address.service';
 import { Address } from './entities/address.entity';
 import { CreateAddressInput } from './dto/create-address.input';
 import { UpdateAddressInput } from './dto/update-address.input';
+import { Roles } from '../Decorators/roles.decorator';
+import { Role } from '../role.enum';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/gql-auth.guard';
+import { RolesGuard } from '../auth/role.guard';
+import { User } from 'src/user/entities/user.entity';
+import { CurrentUser } from '../Decorators/currentUser.decorator';
 
 @Resolver(() => Address)
 export class AddressResolver {
   constructor(private readonly addressService: AddressService) {}
 
   @Mutation(() => Address)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.COMPANY)
   createAddress(
     @Args('createAddressInput') createAddressInput: CreateAddressInput,
+    @CurrentUser() user: User,
   ): Promise<Address> {
     return this.addressService.create(createAddressInput);
   }
@@ -26,16 +36,22 @@ export class AddressResolver {
   }
 
   @Mutation(() => Address)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.COMPANY)
   updateAddress(
     @Args('id', { type: () => Int }) id: number,
     @Args('updateAddressInput') updateAddressInput: UpdateAddressInput,
+    @CurrentUser() user: User,
   ): Promise<Address> | null {
     return this.addressService.update(id, updateAddressInput);
   }
 
   @Mutation(() => Address)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.COMPANY, Role.SUPERADMIN, Role.ADMIN)
   removeAddress(
     @Args('id', { type: () => Int }) id: number,
+    @CurrentUser() user: User,
   ): Promise<Address> | null {
     return this.addressService.remove(id);
   }
--- backend/src/category/category.resolver.ts
+++ backend/src/category/category.resolver.ts
@@ -12,14 +12,24 @@ import { Category } from './entities/category.entity';
 import { CreateCategoryInput } from './dto/create-category.input';
 import { UpdateCategoryInput } from './dto/update-category.input';
 import { Sector } from 'src/sector/entities/sector.entity';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/gql-auth.guard';
+import { RolesGuard } from '../auth/role.guard';
+import { Role } from '../role.enum';
+import { Roles } from '../Decorators/roles.decorator';
+import { User } from '../user/entities/user.entity';
+import { CurrentUser } from '../Decorators/currentUser.decorator';
 
 @Resolver(() => Category)
 export class CategoryResolver {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Mutation(() => Category)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   createCategory(
     @Args('createCategoryInput') createCategoryInput: CreateCategoryInput,
+    @CurrentUser() user: User,
   ): Promise<Category> {
     return this.categoryService.create(createCategoryInput);
   }
@@ -35,16 +45,22 @@ export class CategoryResolver {
   }
 
   @Mutation(() => Category)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   updateCategory(
     @Args('id', { type: () => Int }) id: number,
     @Args('updateCategoryInput') updateCategoryInput: UpdateCategoryInput,
+    @CurrentUser() user: User,
   ): Promise<Category> {
     return this.categoryService.update(id, updateCategoryInput);
   }
 
   @Mutation(() => Category)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   removeCategory(
     @Args('id', { type: () => Int }) id: number,
+    @CurrentUser() user: User,
   ): Promise<Category> | null {
     return this.categoryService.remove(id);
   }
