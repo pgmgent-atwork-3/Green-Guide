3a513430a6e72d79b9e72dede8c2866d401b1fff
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2022-12-18 15:47:02 +0100

    added typing and security to review endpoint

--- backend/src/review/review.resolver.ts
+++ backend/src/review/review.resolver.ts
@@ -3,33 +3,56 @@ import { ReviewService } from './review.service';
 import { Review } from './entities/review.entity';
 import { CreateReviewInput } from './dto/create-review.input';
 import { UpdateReviewInput } from './dto/update-review.input';
+import { Roles } from '../Decorators/roles.decorator';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/gql-auth.guard';
+import { RolesGuard } from '../auth/role.guard';
+import { Role } from '../role.enum';
+import { CurrentUser } from '../Decorators/currentUser.decorator';
+import { User } from '../user/entities/user.entity';
 
 @Resolver(() => Review)
 export class ReviewResolver {
   constructor(private readonly reviewService: ReviewService) {}
 
   @Mutation(() => Review)
-  createReview(@Args('createReviewInput') createReviewInput: CreateReviewInput) {
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.USER)
+  createReview(
+    @Args('createReviewInput') createReviewInput: CreateReviewInput,
+    @CurrentUser() user: User,
+  ): Promise<Review> {
     return this.reviewService.create(createReviewInput);
   }
 
   @Query(() => [Review], { name: 'review' })
-  findAll() {
+  findAll(): Promise<Review[]> {
     return this.reviewService.findAll();
   }
 
   @Query(() => Review, { name: 'review' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  findOne(@Args('id', { type: () => Int }) id: number): Promise<Review> {
     return this.reviewService.findOne(id);
   }
 
   @Mutation(() => Review)
-  updateReview(@Args('updateReviewInput') updateReviewInput: UpdateReviewInput) {
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.USER)
+  updateReview(
+    @Args('updateReviewInput') updateReviewInput: UpdateReviewInput,
+    @Args('id') id: number,
+    @CurrentUser() user: User,
+  ): Promise<Review> {
     return this.reviewService.update(updateReviewInput.id, updateReviewInput);
   }
 
   @Mutation(() => Review)
-  removeReview(@Args('id', { type: () => Int }) id: number) {
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN, Role.USER)
+  removeReview(
+    @Args('id', { type: () => Int }) id: number,
+    @CurrentUser() user: User,
+  ): Promise<Review> {
     return this.reviewService.remove(id);
   }
 }
