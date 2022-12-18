3b8549267ae02c91c228bd87492e7c58995ac3fc
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2022-12-18 17:24:13 +0100

    added typing and security to company-request endpoint

--- backend/src/company-request/company-request.resolver.ts
+++ backend/src/company-request/company-request.resolver.ts
@@ -3,11 +3,19 @@ import { CompanyRequestService } from './company-request.service';
 import { CompanyRequest } from './entities/company-request.entity';
 import { CreateCompanyRequestInput } from './dto/create-company-request.input';
 import { UpdateCompanyRequestInput } from './dto/update-company-request.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/gql-auth.guard';
+import { RolesGuard } from '../auth/role.guard';
+import { Role } from '../role.enum';
+import { Roles } from '../Decorators/roles.decorator';
+import { CurrentUser } from '../Decorators/currentUser.decorator';
+import { User } from '../user/entities/user.entity';
 
 @Resolver(() => CompanyRequest)
 export class CompanyRequestResolver {
   constructor(private readonly companyRequestService: CompanyRequestService) {}
 
+  // TODO need to discus how to secure this
   @Mutation(() => CompanyRequest)
   createCompanyRequest(
     @Args('createCompanyRequestInput')
@@ -29,17 +37,23 @@ export class CompanyRequestResolver {
   }
 
   @Mutation(() => CompanyRequest)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN, Role.COMPANY)
   updateCompanyRequest(
     @Args('id', { type: () => Int }) id: number,
     @Args('updateCompanyRequestInput')
     updateCompanyRequestInput: UpdateCompanyRequestInput,
+    @CurrentUser() user: User,
   ): Promise<CompanyRequest> {
     return this.companyRequestService.update(id, updateCompanyRequestInput);
   }
 
   @Mutation(() => CompanyRequest)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN, Role.COMPANY)
   removeCompanyRequest(
     @Args('id', { type: () => Int }) id: number,
+    @CurrentUser() user: User,
   ): Promise<CompanyRequest> | null {
     return this.companyRequestService.remove(id);
   }
