20cce4d2a58a60ac0d4ce5f70cbb3ea19d2a4095
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2022-12-18 17:07:49 +0100

    added typing and security to company-type endpoint

--- backend/src/company-type/company-type.resolver.ts
+++ backend/src/company-type/company-type.resolver.ts
@@ -3,15 +3,25 @@ import { CompanyTypeService } from './company-type.service';
 import { CompanyType } from './entities/company-type.entity';
 import { CreateCompanyTypeInput } from './dto/create-company-type.input';
 import { UpdateCompanyTypeInput } from './dto/update-company-type.input';
+import { Roles } from '../Decorators/roles.decorator';
+import { Role } from '../role.enum';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/gql-auth.guard';
+import { RolesGuard } from '../auth/role.guard';
+import { CurrentUser } from '../Decorators/currentUser.decorator';
+import { User } from '../user/entities/user.entity';
 
 @Resolver(() => CompanyType)
 export class CompanyTypeResolver {
   constructor(private readonly companyTypeService: CompanyTypeService) {}
 
   @Mutation(() => CompanyType)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   createCompanyType(
     @Args('createCompanyTypeInput')
     createCompanyTypeInput: CreateCompanyTypeInput,
+    @CurrentUser() user: User,
   ): Promise<CompanyType> {
     return this.companyTypeService.create(createCompanyTypeInput);
   }
@@ -27,17 +37,23 @@ export class CompanyTypeResolver {
   }
 
   @Mutation(() => CompanyType)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   updateCompanyType(
     @Args('id', { type: () => Int }) id: number,
     @Args('updateCompanyTypeInput')
     updateCompanyTypeInput: UpdateCompanyTypeInput,
+    @CurrentUser() user: User,
   ) {
     return this.companyTypeService.update(id, updateCompanyTypeInput);
   }
 
   @Mutation(() => CompanyType)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   removeCompanyType(
     @Args('id', { type: () => Int }) id: number,
+    @CurrentUser() user: User,
   ): Promise<CompanyType> | null {
     return this.companyTypeService.remove(id);
   }
