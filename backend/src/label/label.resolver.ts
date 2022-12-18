09a56c8fe90303b4d474c5398f76861d7330d2f4
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2022-12-18 16:56:29 +0100

    added typing and security to label endpoint

--- backend/src/label/label.resolver.ts
+++ backend/src/label/label.resolver.ts
@@ -3,14 +3,24 @@ import { LabelService } from './label.service';
 import { Label } from './entities/label.entity';
 import { CreateLabelInput } from './dto/create-label.input';
 import { UpdateLabelInput } from './dto/update-label.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/gql-auth.guard';
+import { RolesGuard } from '../auth/role.guard';
+import { Role } from '../role.enum';
+import { Roles } from '../Decorators/roles.decorator';
+import { User } from '../user/entities/user.entity';
+import { CurrentUser } from '../Decorators/currentUser.decorator';
 
 @Resolver(() => Label)
 export class LabelResolver {
   constructor(private readonly labelService: LabelService) {}
 
   @Mutation(() => Label)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   createLabel(
     @Args('createLabelInput') createLabelInput: CreateLabelInput,
+    @CurrentUser() user: User,
   ): Promise<Label> {
     return this.labelService.create(createLabelInput);
   }
@@ -26,16 +36,22 @@ export class LabelResolver {
   }
 
   @Mutation(() => Label)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   updateLabel(
     @Args('id', { type: () => Int }) id: number,
     @Args('updateLabelInput') updateLabelInput: UpdateLabelInput,
+    @CurrentUser() user: User,
   ): Promise<Label> {
     return this.labelService.update(id, updateLabelInput);
   }
 
   @Mutation(() => Label)
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   removeLabel(
     @Args('id', { type: () => Int }) id: number,
+    @CurrentUser() user: User,
   ): Promise<Label> | null {
     return this.labelService.remove(id);
   }
