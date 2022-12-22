ae7672665b332ac902aef683eb1625f7fd26ed54
Jef Leroux <91065654+pgm-jeflerou@users.noreply.github.com>
2022-12-22 10:58:33 +0100

    linked login on web to backend (you now have a token for the backend in your local storage)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(csurf());
   app.enableCors();
   await app.listen(3001);
 }
