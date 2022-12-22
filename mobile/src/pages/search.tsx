4aebc8321bee149d164b3a5924b8c133993c4816
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-12-22 14:50:38 +0100

    Created search page

---
+++ mobile/src/pages/search.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const search = () => {
+  return (
+    <div>
+        <h1>Search</h1>
+        <Navbar />
+        <hr />
+        <ol>
+            <li>header</li>
+            <li>search bar</li>
+            <li>filter</li>
+            <li>map</li>
+            <li>vendor addresses</li>
+        </ol>
+    </div>
+  )
+}
+
+export default search
