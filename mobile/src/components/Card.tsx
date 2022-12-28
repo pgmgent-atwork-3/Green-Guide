721bab48584916fbd8f34f047575156c5aab2f3e
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-12-28 19:35:00 +0100

    Made a Card component

---
+++ mobile/src/components/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../../styles/Home.module.scss'
+
+const Card = () => {
+return (
+    <div className={styles.card}>
+        <p>The Green Guide is your eco personal guide through Ghent! With this app, you can easily and quickly navigate yourself through our green city. Find sustainable and environmentally conscious places in no time! </p>
+
+    </div>
+)
+}
+
+export default Card
