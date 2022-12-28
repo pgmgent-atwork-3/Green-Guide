1153ce6f46171ef60c8150ad24022a5519d69825
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-12-28 11:46:15 +0100

    Update register.tsx

--- mobile/src/pages/register.tsx
+++ mobile/src/pages/register.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useRef, useState } from 'react';
-
+import { useEffect, useRef, useState } from 'react'
+import styles from '../../styles/Home.module.scss'
 const Register = () => {
     const container = useRef(null);
     const logoDimensions = {
@@ -12,36 +12,54 @@ const Register = () => {
     const [width, setWidth] = useState(480);
     const [height, setHeight] = useState(800 / 5.3);
 
+    // useEffect(() => {
+    //     if (container.current !== null){
+    //         setWidth(container.current.offsetWidth);
+    //         setHeight(container.current.offsetHeight/5.3);
+    //     }
+    // }, []);
+
     return (
-        <div className='app-container' ref={container}>
-            <Image src='/Blob.png' width={width} height={height} alt='A decorative background' className='blob'/>
-            <Image src='/Logo.png' width={logoDimensions.width} height={logoDimensions.height} alt='Green Guide logo' className='logo' style={{ marginTop: height-76, marginLeft: (width-logoDimensions.width)/2 }}/>
+        <div className={styles.app_container} ref={container}>
+            <Image src='/Blob.png' width={width} height={height} alt='A decorative background' className={styles.blob}/>
+            <Image src='/Logo.png' width={logoDimensions.width} height={logoDimensions.height} alt='Green Guide logo' className={styles.logo} style={{ marginTop: height-76, marginLeft: (width-logoDimensions.width)/2 }}/>
             
-            <div className='content-container'>
-                <h1>Register</h1>
-                <form action="">
+            <div className={styles.content_container}>
+                <h1 className={styles.title}>Register</h1>
+                <form className={styles.form} action="">
+
                     <label htmlFor="">First name</label>
-                    <input type="text" name="text" id="firstName" placeholder="First name"/>
+                    <input className={styles.input_field} type="text" name="text" id="firstName" placeholder="First name"/>
+
+
+                    
 
                     <label htmlFor="">Last name</label>
-                    <input type="text" name="text" id="lastName" placeholder="Last name"/>
+                    <input className={styles.input_field} type="text" name="text" id="lastName" placeholder="Last name"/>
 
                     <label htmlFor="">E-mail</label>
-                    <input type="email" name="email" id="email" placeholder="E-mail"/>
+                    <input className={styles.input_field} type="email" name="email" id="email" placeholder="E-mail"/>
 
                     <label htmlFor="">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Password"/>
+                    <input className={styles.input_field} type="password" name="password" id="password" placeholder="Password"/>
 
                     <label htmlFor="">Birth date</label>
-                    <input type="date" name="date" id="date" placeholder="Date"/>
+                    <input  className={styles.input_field} type="date" name="date" id="date" placeholder="Date"/>
 
                     <label htmlFor="">Phone number</label>
-                    <input type="text" name="phoneNumber" id="phoneNumber" placeholder="Phone number"/>
+                    <input  className={styles.input_field} type="text" name="phoneNumber" id="phoneNumber" placeholder="Phone number"/>
 
-                    <button className='btn btn-primary'>Register</button>
+                    <div className="form_row">
+                        <input className={styles.checkbox} type="checkbox" name="remember" id="remember"/>
+                        <label htmlFor="remember">Remember me</label>
+                    </div>
+
+                    <button className={`${styles.btn} ${styles.btn_secondary}`}>Register with Google</button>
+
+                    <button className={`${styles.btn} ${styles.btn_primary}`}>Register</button>
                 </form>
-                <span>Already have an account?</span>
-                <button className='btn btn-secondary'><a href="/login">Login</a></button>
+                <span className={styles.grey_text}>Already have an account yet?</span>
+                <button className={`${styles.btn} ${styles.btn_secondary}`}><a className={styles.link} href="/login">Login</a></button>
             </div>
         </div>
     )
