f67f6cc7795182d3fdb4baccfd750973eedbef1f
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-12-28 11:47:13 +0100

    Update _app.tsx

--- mobile/_app.tsx
+++ mobile/_app.tsx
@@ -1,6 +1,3 @@
-// import '../src/scss/main.scss'
-// import './styles/Home.module.css'
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
