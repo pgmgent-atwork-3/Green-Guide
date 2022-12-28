258c043551a9fe7e207250f7eeef190733852d3c
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-12-28 11:46:10 +0100

    Update index.tsx

--- mobile/src/pages/index.tsx
+++ mobile/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useRef, useState } from 'react'
-import styles from '../../styles/Home.module.css'
+import { useEffect, useRef, useState } from 'react'
+import styles from '../../styles/Home.module.scss'
 
 const Home = () => {
     const container = useRef(null);
