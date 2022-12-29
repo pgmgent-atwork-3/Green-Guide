904b2cd83808fbb0e29d95a788da27bf19e7fc92
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-12-29 10:29:44 +0100

    Added profile content + basic styling

--- mobile/src/pages/profile.tsx
+++ mobile/src/pages/profile.tsx
@@ -3,41 +3,53 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useRef, useState } from 'react'
 import styles from '../../styles/Home.module.scss'
+import Header from '../components/Header'
 
 const profile = () => {
     const container = useRef(null);
-    const logoDimensions = {
-        width: 98,
-        height: 98,
+    const profileDimensions = {
+        width: 60,
+        height: 60,
     }
 
-    const [width, setWidth] = useState(480);
-    const [height, setHeight] = useState(800 / 5.3);
-
-    // useEffect(() => {
-    //     if (container.current !== null){
-    //         setWidth(container.current.offsetWidth);
-    //         setHeight(container.current.offsetHeight/5.3);
-    //     }
-    // }, []);
+    const iconDimensions = {
+        width: 15,
+        height: 15,
+    }
 
 return (
     <div className={styles.app_container} ref={container}>
-        <Image src='/Blob.png' width={width} height={height} alt='A decorative background' className={styles.blob}/>
-        <Image src='/Logo.png' width={logoDimensions.width} height={logoDimensions.height} alt='Green Guide logo' className={styles.logo} style={{ marginTop: height-76, marginLeft: (width-logoDimensions.width)/2 }}/>
-        
+        <Header />    
         <Navbar />
         <div className={styles.content_container}>
-            <h1 className={styles.title}>Profile</h1>
-            
-            <div className={styles.btn_group}>
-                <ol>
-                    <li>header</li>
-                    <li>profile image and name</li>
-                    <li>settings block</li>
-                    <li>sign off button</li>
-                </ol>
+
+            <div className={styles.profile_header}>
+                <Image className={styles.profile_image} src="/svg/profile.svg" width={profileDimensions.width} height={profileDimensions.height} alt="profile-img"/>
+                <h1 className={styles.title}>Jane Doe</h1>
             </div>
+
+            {/* this card needs to become a component with type = settings-card */}
+            <div className={styles.settings_card}>
+                <div className={styles.profile_settings_item}>
+                    <h2 className={styles.subtitle}>Profile details</h2>
+                    <Image className={styles.profile_settings_icon} src="/svg/arrow.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
+                </div>
+                <div className={styles.profile_settings_item}>
+                    <h2 className={styles.subtitle}>My reviews</h2>
+                    <Image className={styles.profile_settings_icon} src="/svg/arrow.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
+                </div>
+                <div className={styles.profile_settings_item}>
+                    <h2 className={styles.subtitle}>Favorite vendors</h2>
+                    <Image className={styles.profile_settings_icon} src="/svg/arrow.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
+                </div>
+                <div className={styles.profile_settings_item}>
+                    <h2 className={styles.subtitle}>Settings</h2>
+                    <Image className={styles.profile_settings_icon} src="/svg/arrow.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
+                </div>
+            </div>
+
+            {/* sign off button */}
+            <button className={`${styles.btn} ${styles.btn_primary}`}>Sign off</button>
         </div>
     </div>
 )
