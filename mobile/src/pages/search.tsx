a3b369d240b0357d5e71fd72d9ea7ff584f8e92a
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-12-29 10:30:18 +0100

    Added Search content + basic styling

--- mobile/src/pages/search.tsx
+++ mobile/src/pages/search.tsx
@@ -3,41 +3,70 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useRef, useState } from 'react'
 import styles from '../../styles/Home.module.scss'
+import Header from '../components/Header'
 
 const search = () => {
     const container = useRef(null);
     const logoDimensions = {
-        width: 98,
-        height: 98,
+        width: 20,
+        height: 20,
     }
 
-    const [width, setWidth] = useState(480);
-    const [height, setHeight] = useState(800 / 5.3);
+    const mapDimensions = {
+        width: 400,
+        height: 200,
+    }
 
-    // useEffect(() => {
-    //     if (container.current !== null){
-    //         setWidth(container.current.offsetWidth);
-    //         setHeight(container.current.offsetHeight/5.3);
-    //     }
-    // }, []);
 
 return (
     <div className={styles.app_container} ref={container}>
-        <Image src='/Blob.png' width={width} height={height} alt='A decorative background' className={styles.blob}/>
-        <Image src='/Logo.png' width={logoDimensions.width} height={logoDimensions.height} alt='Green Guide logo' className={styles.logo} style={{ marginTop: height-76, marginLeft: (width-logoDimensions.width)/2 }}/>
-        
+        <Header />
+
         <Navbar />
         <div className={styles.content_container}>
-            <h1 className={styles.title}>Search</h1>
-            
-            <div className={styles.btn_group}>
-                <ol>
-                    <li>header</li>
-                    <li>search bar</li>
-                    <li>filter</li>
-                    <li>map</li>
-                    <li>vendor addresses</li>
-                </ol>
+
+            <div className={styles.search_field}>
+                {/* filter */}
+                <div className={styles.filter}>
+                    <div className={styles.filter_icon}>
+                        <Image className={styles.filter_item} src='/svg/filter.svg' width={logoDimensions.width} height={logoDimensions.height} alt='Filter'/>
+                    </div>
+                </div>
+
+                {/* search bar */}
+                <div className={styles.search_bar}>
+                    <input type="text" placeholder="Search for a vendor" className={styles.search_input}/>
+                    <button className={styles.search_button}>
+                        <Image src='/svg/search-glass.svg' width={logoDimensions.width} height={logoDimensions.height} alt='Search'/>
+                    </button>
+                </div>
+            </div>
+
+            {/* map */}
+            <div className={styles.map}>
+                <Image src='/svg/map.svg' width={mapDimensions.width} height={mapDimensions.height} alt='Map'/>
+            </div>
+
+            {/* vendor addresses */}
+            <div className={styles.reward_card}>
+                <a href="/vendor-detail">
+                    <h2 className={styles.title}>Ohne</h2>            
+                    <p className={styles.subtitle}>Steendam 68, 9000 Gent</p>
+                </a>
+            </div>
+
+            <div className={styles.reward_card}>
+                <a href="/vendor-detail">
+                    <h2 className={styles.title}>Tasty World</h2>            
+                    <p className={styles.subtitle}>Reep 14B, 9000 Gent</p>
+                </a>
+            </div>
+
+            <div className={styles.reward_card}>
+                <a href="/vendor-detail">
+                    <h2 className={styles.title}>Appelier</h2>            
+                    <p className={styles.subtitle}>Oude Houtlei 122, 9000 Gent</p>
+                </a>
             </div>
         </div>
     </div>
