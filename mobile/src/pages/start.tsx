2b41ed0686638d12d1bb6daff6eabc401ff869f0
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2023-01-04 20:52:44 +0100

    Update start.tsx

--- mobile/src/pages/start.tsx
+++ mobile/src/pages/start.tsx
@@ -31,33 +31,30 @@ const Start = () => {
     // }, []);
 
 return (
-    <div className={styles.app_container} ref={container}>
-        <Header />
-        <Navbar />
-        <div className={styles.content_container}>
-            <Card/>
-
-            <h2 className={styles.title}>Popular Green vendors</h2>
-            <p className={styles.subtitle}>Give them some love from us!</p>
-            <Carousel/>
-
-            <Card/>
-
-            <h2 className={styles.title}>Newest Green vendors</h2>
-            <p className={styles.subtitle}>Give them a warm welcome from us!</p>
-            <Carousel/>
-
-            {/* this card need to become a component with type = info-card */}
-            <div className={`${styles.info_card} ${styles.flex_row}`}>
-                <Image className={styles.img} src="/svg/socials/fb.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
-                <Image className={styles.img} src="/svg/socials/msg.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
-                <Image className={styles.img} src="/svg/socials/ig.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
-                <Image className={styles.img} src="/svg/socials/tt.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
-                <Image className={styles.img} src="/svg/socials/sc.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
-            </div>
+<div className={styles.app_container} ref={container}>
+    <Header />
+    <Navbar />
+    <div className={styles.content_container}>
+        <Card/>
+        <h2 className={styles.title}>Popular Green vendors</h2>
+        <p className={styles.subtitle}>Give them some love from us!</p>
+        <Carousel/>
+        <Card/>
+        <h2 className={styles.title}>Newest Green vendors</h2>
+        <p className={styles.subtitle}>Give them a warm welcome from us!</p>
+        <Carousel/>
+        {/* this card need to become a component with type = info-card */}
+        <div className={`${styles.info_card} ${styles.flex_row}`}>
+            <Image className={styles.img} src="/svg/socials/fb.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
+            <Image className={styles.img} src="/svg/socials/msg.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
+            <Image className={styles.img} src="/svg/socials/ig.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
+            <Image className={styles.img} src="/svg/socials/tt.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
+            <Image className={styles.img} src="/svg/socials/sc.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
         </div>
     </div>
-    )
-}
+</div>
+)
+};
+
 
 export default Start
