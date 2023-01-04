6e0740e1dadf75946ca4a217546984f4dbd9dc02
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2023-01-04 15:34:52 +0100

    Added Reward Card + detail content

--- mobile/src/pages/score.tsx
+++ mobile/src/pages/score.tsx
@@ -12,6 +12,11 @@ const score = () => {
         height: 98,
     }
 
+    const iconDimensions = {
+        width: 15,
+        height: 15,
+    }
+
     const [width, setWidth] = useState(480);
     const [height, setHeight] = useState(800 / 5.3);
 
@@ -36,25 +41,73 @@ return (
 
             {/* this card need to become a component with type = reward-card */}
             <div className={styles.reward_card}>
-                <p className={styles.subtitle}>You collected <span>72</span> points at</p>
-                <h2 className={styles.title}>Ohne</h2>            
+                <div className={styles.reward_header}>
+                    <div className={styles.header}>
+                        <p className={styles.subtitle}>You collected <span className={styles.accent_color}>72</span> points at</p>
+                        <h2 className={styles.title}>Ohne</h2>   
+                    </div>
+                    <Image className={styles.img} src="/svg/arrow.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
+                </div>
+                <div className={styles.flex_row}>
+                    <h3 className={styles.h3}>Collect rewards</h3>
+                    <Image src="/svg/info.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
+                </div>
+                <div className={styles.rewards}>
+                    <div className={styles.reward}>
+                        <div className={styles.points_bubble}>
+                            <p className={styles.points}>10 <br /> pnts</p>
+                        </div>
+                        <p className={styles.point_name}>Tote bag</p>
+                    </div>
+                    <div className={styles.reward}>
+                        <div className={styles.points_bubble}>
+                            <p className={styles.points}>10 <br /> pnts</p>
+                        </div>
+                        <p className={styles.point_name}>Free coffee</p>
+                    </div>
+                    <div className={styles.reward}>
+                        <div className={styles.points_bubble}>
+                            <p className={styles.points}>10 <br /> pnts</p>
+                        </div>
+                        <p className={styles.point_name}>Dessert</p>
+                    </div>
+                    <div className={styles.reward}>
+                        <div className={styles.points_bubble}>
+                            <p className={styles.points}>10 <br /> pnts</p>
+                        </div>
+                        <p className={styles.point_name}>Tote bag</p>
+                    </div>
+                    <div className={styles.reward}>
+                        <div className={styles.points_bubble}>
+                            <p className={styles.points}>10 <br /> pnts</p>
+                        </div>
+                        <p className={styles.point_name}>Free coffee</p>
+                    </div>
+                    <div className={styles.reward}>
+                        <div className={styles.points_bubble}>
+                            <p className={styles.points}>10 <br /> pnts</p>
+                        </div>
+                        <p className={styles.point_name}>Dessert</p>
+                    </div>
+                </div>
+                <p className={styles.grey_text}>Show more rewards</p>
             </div>
 
             <div className={styles.reward_card}>
-                <p className={styles.subtitle}>You collected <span>15</span> points at</p>
-                <h2 className={styles.title}>Tasty World</h2>            
+                <div className={styles.card_content}>
+                    <p className={styles.subtitle}>You collected <span className={styles.accent_color}>15</span> points at</p>
+                    <h2 className={styles.title}>Tasty World</h2>  
+                </div>
+                <Image className={styles.img} src="/svg/arrow.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
             </div>
 
             <div className={styles.reward_card}>
-                <p className={styles.subtitle}>You collected <span>10</span> points at</p>
-                <h2 className={styles.title}>Appelier</h2>            
+                <div className={styles.card_content}>
+                    <p className={styles.subtitle}>You collected <span className={styles.accent_color}>10</span> points at</p>
+                    <h2 className={styles.title}>Appelier</h2>   
+                </div>
+                <Image className={styles.img} src="/svg/arrow.svg" width={iconDimensions.width} height={iconDimensions.height} alt="profile-img"/>
             </div>
-            
-            
-                <ol>            
-                    <li>details of the rewards</li>
-                </ol>
-            
         </div>
     </div>
 )
