2f7cf76d9e0b256c4183fdd4524f77583d1d4550
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2023-01-04 11:55:37 +0100

    Update Signup.jsx

--- web/src/pages/Signup.jsx
+++ web/src/pages/Signup.jsx
@@ -301,7 +301,6 @@ return (
                 </div>
             </section>
 
-
             {/* Green Guide Labels */}
             <section className="gg-labels">
                 <div className="mb-8 shadow sm:overflow-hidden sm:rounded-md">
@@ -452,7 +451,7 @@ return (
                                         type="submit"
                                         // disabled={isSubmitting}
                                         className="mt-5 inline-flex justify-center rounded-md border border-transparent bg-orange-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                    >Save</button>
+                                    >Send request</button>
                                 </div>
                             </div>
                         </div>
