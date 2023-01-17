363d57fbf8282a9fc53cbafb0aabee1410596bad
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-01-17 15:23:31 +0100

    Changed <a> to <Link> in Navbar

--- mobile/src/components/Navbar.tsx
+++ mobile/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../../styles/Home.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
     const iconDimensions = {
@@ -12,22 +13,22 @@ return (
         <ul className={styles.navbar}>
             <li className={styles.nav_item}>
                 <Image src='/svg/start.svg' width={iconDimensions.width} height={iconDimensions.height} alt='start-icon' className={styles.nav_icon}/>
-                <a className={styles.nav_link} href="/start">start</a>
+                <Link className={styles.nav_link} href="/start">start</Link>
             </li>
             <li className={styles.nav_item}>
                 <Image src='/svg/score.svg' width={iconDimensions.width} height={iconDimensions.height} alt='score-icon' className={styles.nav_icon}/>
-                <a className={styles.nav_link} href="/score">score</a>
+                <Link className={styles.nav_link} href="/score">score</Link>
             </li>
             <li className={`${styles.nav_item} ${styles.green_icon}`}>
-                <a className={styles.nav_link} href="/scan"><Image src='/svg/scan.svg' width={iconDimensions.width} height={iconDimensions.height} alt='scan-icon' className={styles.nav_icon}/></a>
+                <Link className={styles.nav_link} href="/scan"><Image src='/svg/scan.svg' width={iconDimensions.width} height={iconDimensions.height} alt='scan-icon' className={styles.nav_icon}/></Link>
             </li>
             <li className={styles.nav_item}>
                 <Image src='/svg/search.svg' width={iconDimensions.width} height={iconDimensions.height} alt='search-icon' className={styles.nav_icon}/>
-                <a className={styles.nav_link} href="/search">search</a>
+                <Link className={styles.nav_link} href="/search">search</Link>
             </li>
             <li className={styles.nav_item}>
                 <Image src='/svg/profile.svg' width={iconDimensions.width} height={iconDimensions.height} alt='profile-icon' className={styles.nav_icon}/>
-                <a className={styles.nav_link} href="/profile">profile</a>
+                <Link className={styles.nav_link} href="/profile">profile</Link>
             </li>
         </ul>
     </div>
