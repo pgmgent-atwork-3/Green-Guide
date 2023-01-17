8b3e76ee32bacfcccbfeabf75ae554adf681c1f2
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-01-17 14:24:25 +0100

    Added connection to backend and updated login

--- mobile/src/pages/login.tsx
+++ mobile/src/pages/login.tsx
@@ -1,3 +1,4 @@
+import { Formik, Form, Field } from 'formik'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useRef, useState } from 'react'
@@ -27,23 +28,44 @@ const Login = () => {
             
             <div className={`${styles.content_container} ${styles.spaced}`}>
                 <h1 className={styles.title}>Login</h1>
-                <form className={styles.form} action="">
-                    <label htmlFor="">E-mail</label>
-                    <input className={styles.input_field} type="email" name="email" id="email" placeholder="E-mail"/>
+                <Formik
+                    initialValues={{
+                        email: '',
+                        password: '',
+                        remember: false,
+                    }}
+                    onSubmit={async (values, { setSubmitting }) => {
+                        await fetch("http://localhost:3001/auth/login", {
+                          method: "POST",
+                          headers: {
+                            "Content-Type": "application/json",
+                          },
+                          credentials: "include",
+                          body: JSON.stringify(values),
+                        })
+                          .then((res) => res.json())
+                          .catch((err) => console.error(err));
+                        setSubmitting(false);
+                      }}
+                >
+                    <Form className={styles.form} action="">
+                        <label htmlFor="email">E-mail</label>
+                        <Field className={styles.input_field} type="email" name="email" id="email" placeholder="E-mail"/>
 
-                    <label htmlFor="">Password</label>
-                    <input className={styles.input_field} type="password" name="password" id="password" placeholder="Password"/>
+                        <label htmlFor="password">Password</label>
+                        <Field className={styles.input_field} type="password" name="password" id="password" placeholder="Password"/>
 
-                    <div className="form_row">
-                        <input className={styles.checkbox} type="checkbox" name="remember" id="remember"/>
-                        <label htmlFor="remember">Remember me</label>
-                    </div>
+                        <div className="form_row">
+                            <Field className={styles.checkbox} type="checkbox" name="remember" id="remember"/>
+                            <label htmlFor="remember">Remember me</label>
+                        </div>
 
-                    <span  className={styles.grey_text}>Forgot your password?</span>
-                    <button className={`${styles.btn} ${styles.btn_secondary}`}> <a href="/start">Login with Google</a> </button>
+                        <span  className={styles.grey_text}>Forgot your password?</span>
+                        <button className={`${styles.btn} ${styles.btn_secondary}`}> <a href="/start">Login with Google</a> </button>
 
-                    <button className={`${styles.btn} ${styles.btn_primary}`}>Login</button>
-                </form>
+                        <button type="submit" className={`${styles.btn} ${styles.btn_primary}`}>Login</button>
+                    </Form>
+                </Formik>
                 <span className={styles.grey_text}>Dont have an account?</span>
                 <button className={`${styles.btn} ${styles.btn_secondary}`}><a className={styles.link} href="/register">Register</a></button>
             </div>
