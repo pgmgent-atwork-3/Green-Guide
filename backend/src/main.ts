6babae046384a18b1332b3af0e0003b4818bad23
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-01-19 11:40:32 +0100

    Changed code to match last succesful deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,11 +7,15 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.MOBILE_URL,
+    origin: [process.env.WEB_URL, process.env.MOBILE_URL],
     credentials: true,
   });
   app.use(cookieParser());
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', [
+      process.env.WEB_URL,
+      process.env.MOBILE_URL,
+    ]);
+  });
   await app.listen(3001);
 }
-
-bootstrap();
