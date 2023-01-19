5a27138e6eaf05b3128dc685b864bb57bb357e0f
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-01-19 11:34:40 +0100

    Tried fixing cors allow-origin error on deployed version

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,15 +7,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.WEB_URL, process.env.MOBILE_URL],
+    origin: process.env.MOBILE_URL,
     credentials: true,
   });
   app.use(cookieParser());
-  app.use((req, res, next) => {
-    if (req.headers.origin) {
-      res.header('Access-Control-Allow-Origin', req.headers.origin);
-    }
-  });
   await app.listen(3001);
 }
 
