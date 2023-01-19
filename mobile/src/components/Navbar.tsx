fd62cc52e20deb37311a46ef7c3e871d2b71ea17
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2023-01-19 20:59:36 +0100

    Update Navbar.tsx

--- mobile/src/components/Navbar.tsx
+++ mobile/src/components/Navbar.tsx
@@ -11,25 +11,39 @@ const Navbar = () => {
 return (
     <div>
         <ul className={styles.navbar}>
-            <li className={styles.nav_item}>
-                <Image src='/svg/start.svg' width={iconDimensions.width} height={iconDimensions.height} alt='start-icon' className={styles.nav_icon}/>
-                <Link className={styles.nav_link} href="/start">start</Link>
-            </li>
-            <li className={styles.nav_item}>
-                <Image src='/svg/score.svg' width={iconDimensions.width} height={iconDimensions.height} alt='score-icon' className={styles.nav_icon}/>
-                <Link className={styles.nav_link} href="/score">score</Link>
-            </li>
-            <li className={`${styles.nav_item} ${styles.green_icon}`}>
-                <Link className={styles.nav_link} href="/scan"><Image src='/svg/scan.svg' width={iconDimensions.width} height={iconDimensions.height} alt='scan-icon' className={styles.nav_icon}/></Link>
-            </li>
-            <li className={styles.nav_item}>
-                <Image src='/svg/search.svg' width={iconDimensions.width} height={iconDimensions.height} alt='search-icon' className={styles.nav_icon}/>
-                <Link className={styles.nav_link} href="/search">search</Link>
-            </li>
-            <li className={styles.nav_item}>
-                <Image src='/svg/profile.svg' width={iconDimensions.width} height={iconDimensions.height} alt='profile-icon' className={styles.nav_icon}/>
-                <Link className={styles.nav_link} href="/profile">profile</Link>
-            </li>
+            <Link className={styles.nav_link} href="/start">
+                <li className={styles.nav_item}>
+                    <Image src='/svg/start.svg' width={iconDimensions.width} height={iconDimensions.height} alt='start-icon' className={styles.nav_icon}/>
+                    start
+                </li>
+            </Link>
+
+            <Link className={styles.nav_link} href="/score">
+                <li className={styles.nav_item}>
+                    <Image src='/svg/score.svg' width={iconDimensions.width} height={iconDimensions.height} alt='score-icon' className={styles.nav_icon}/>
+                    score
+                </li>
+            </Link>
+
+            <Link className={styles.nav_link} href="/scan">
+                <li className={`${styles.nav_item} ${styles.green_icon}`}>
+                    <Image src='/svg/scan.svg' width={iconDimensions.width} height={iconDimensions.height} alt='scan-icon' className={styles.nav_icon}/>
+                </li>
+            </Link>
+
+            <Link className={styles.nav_link} href="/search">
+                <li className={styles.nav_item}>
+                    <Image src='/svg/search.svg' width={iconDimensions.width} height={iconDimensions.height} alt='search-icon' className={styles.nav_icon}/>
+                    search
+                </li>
+            </Link>
+
+            <Link className={styles.nav_link} href="/profile">
+                <li className={styles.nav_item}>
+                    <Image src='/svg/profile.svg' width={iconDimensions.width} height={iconDimensions.height} alt='profile-icon' className={styles.nav_icon}/>
+                    profile
+                </li>
+            </Link>
         </ul>
     </div>
     )
