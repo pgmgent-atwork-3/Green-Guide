eb350de07a018c28fccc3ffb2d26de81759fe503
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2023-01-23 11:48:32 +0100

    Update Header.tsx

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import styles from '../../styles/Home.module.scss'
 
 const Header = () => {
@@ -13,13 +13,6 @@ const Header = () => {
     const [width, setWidth] = useState(480);
     const [height, setHeight] = useState(560 / 5.3);
 
-    // useEffect(() => {
-    //     if (container.current !== null){
-    //         setWidth(container.current.offsetWidth);
-    //         setHeight(container.current.offsetHeight/5.3);
-    //     }
-    // }, []);
-
 return (
     <div>
         <div className={styles.header_text}>
@@ -27,7 +20,7 @@ return (
             <h1 className={styles.title}>The Green Guide</h1>
         </div>
         <Image src='/Blob.png' width={width} height={height} alt='A decorative background' className={styles.blob}/>
-        <Image src='/Logo.png' width={logoDimensions.width} height={logoDimensions.height} alt='Green Guide logo' className={styles.logo} style={{ marginTop: height-85, marginLeft: (width-logoDimensions.width)/1.4 }}/>
+        <Image src='/Logo.png' width={logoDimensions.width} height={logoDimensions.height} alt='Green Guide logo' className={styles.logo_left}/>
     </div>
 )
 }
