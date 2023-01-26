f8ffa6c6ad7cf3a7dda246e5c47fbd2ae53e0cff
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-01-26 14:21:56 +0100

    Improved code in _app.tsx

--- mobile/src/pages/_app.tsx
+++ mobile/src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from 'next/app'
 import '../../styles/base/reset.scss';
+import Navbar from '../components/Navbar';
 
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+    return (
+        <div>
+            <Component {...pageProps} />
+            {/* 
+                Add this line to show the navbar on every page, without loading it on every page
+                <Navbar /> 
+            */}
+        </div>
+    );
 }
+
+export default App;
